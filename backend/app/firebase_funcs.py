cbcaca3a8e4b744dca734d47ecc6f8971ac16f26
Elliot <elliot.jackson180@gmail.com>
2025-08-01 17:44:25 +0100

    updating the way usernames are saved to firebase

--- backend/app/firebase_funcs.py
+++ backend/app/firebase_funcs.py
@@ -9,35 +9,53 @@ def initialise_firebase(path):
             "databaseURL": "https://real-time-leaderboard-3c6d6-default-rtdb.europe-west1.firebasedatabase.app/"
         })
     except FileNotFoundError:
-        print("[ERROR] initialise_firebase: File path is incorrect or the file does not exist")
+        print("[ERROR] firebase_funcs.py/initialise_firebase: File path is incorrect or the file does not exist")
     except Exception as e:
-        print(f"[ERROR] initialise_firebase: {e}")
+        print(f"[ERROR] firebase_funcs.py/initialise_firebase: {e}")
 
 
+# Authenticates the JWT token
 async def authenticate_token(token, app_firebase) -> bool:
     try:
         # Decoding without an error means it is verified :)
         decoded_token = auth.verify_id_token(token, app_firebase)
         return True
     except Exception as e:
-        print(f"[ERROR] authenticate_token: {e}")
+        print(f"[ERROR] firebase_funcs.py/authenticate_token: {e}")
         return False
 
 
+# Updates the score corresponding to the user uid
 async def update_score_db(uid, score):
     data = {
-        "userUID": uid,
         "score": score
     }
-    ref = db.reference("/scores")
-    entry_ref = ref.push(data)
-    print(f"Success: {entry_ref.key}")
+    ref = db.reference(f"/users/{uid}")
+    try:
+        ref.update(data)
+    except Exception as e:
+        print(f"[ERROR] firebase_funcs.py/update_score_db.py: {e}")
 
-async def update_user_db(uid, user):
+
+# Sets the username of the corresponding user uid upon account creation
+async def init_user_db(uid, email, user):
     data = {
-        "userUID": uid,
-        "username": user
+        "username": user,
+        "email": email,
+        "score": 0
     }
-    ref = db.reference("/users")
-    entry_ref = ref.push(data)
-    print(f"Success: {entry_ref}")
+    ref = db.reference(f"/users/{uid}")
+    try:
+        ref.set(data)
+    except Exception as e:
+        print(f"[ERROR] firebase_funcs.py/init_user_db: {e}")
+
+
+# Gets the username of the corresponding user uid
+async def get_user_name_db(user_uid):
+    ref = db.reference(f"/users/{user_uid}/username")
+    try:
+        return ref.get()
+    except Exception as e:
+        print(f"[ERROR] firebase_funcs.py/get_user_name_db: {e}")
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -19,9 +19,44 @@ app.add_middleware(
 )
 
 
+class UserNameSubmission(BaseModel):
+    user_name: str
+    email: str
+    user_uid: str
+
+@app.post("/username/add")
+async def init_user(data: UserNameSubmission):
+    user = data.user_name
+    email = data.email
+    uid = data.user_uid
+    try:
+        await firebase_funcs.init_user_db(uid, email, user)
+        return {user}
+    except Exception as e:
+        print(f"[ERROR] main.py/init_user: {e}")
+
+
+class Token(BaseModel):
+    auth_token: str
+
+@app.post("/username/get/{user_uid}")
+async def get_user_name(user_uid: str, data: Token):
+    token = data.auth_token
+    uid = user_uid
+    auth: bool = await firebase_funcs.authenticate_token(token, app_firebase)
+    if auth:
+        try:
+            user = await firebase_funcs.get_user_name_db(uid)
+            return user
+        except Exception as e:
+            print(f"[ERROR] main.py/get_user_name: {e}")
+    else:
+        return "failure to verify token"
+
+
 class ScoreSubmission(BaseModel):
     score_submitted: int
-    auth_token: object
+    auth_token: str
     user_uid: str
 
 # If someone runs a POST request at /score, run this function
@@ -33,27 +68,18 @@ async def score_submission(data: ScoreSubmission):
 
     auth: bool = await firebase_funcs.authenticate_token(token, app_firebase)
     if auth:
-        await firebase_funcs.update_score_db(uid, score_submitted)
-        return {"status": "success", "receivedScore": score_submitted}
+        try:
+            await firebase_funcs.update_score_db(uid, score_submitted)
+            return {"status": "success", "receivedScore": score_submitted}
+        except Exception as e:
+            print(f"[ERROR] main.py/score_submission: {e}")
     else:
         return {"status": "false", "receivedScore": score_submitted}
-    
-class UserNameSubmission(BaseModel):
-    user_name: str
-    user_uid: str
-
-@app.post("/add-username")
-async def add_user_name(data: UserNameSubmission):
-    user = data.user_name
-    uid = data.user_uid
-    try:
-        await firebase_funcs.update_user_db(uid, user)
-        return {"status": "worked", "user": user}
-    except Exception as e:
-        print(f"[ERROR] add_user_name: {e}")
 
 
 # Endpoint
 @app.get("/")
 async def root():
     return {"message": "API is running"}
+
+    
