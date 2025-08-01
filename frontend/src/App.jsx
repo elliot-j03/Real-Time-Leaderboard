d6687a4e3c71a0ba022b155b989333a61dadcb38
Elliot <elliot.jackson180@gmail.com>
2025-08-01 17:46:39 +0100

    top level app listens to changes in the firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 // React
+import { useEffect, useState } from 'react';
+import { onValue, ref } from "firebase/database";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Firebase
+import { auth, db } from './config/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 // CSS
 import './App.css'
 // Pages
@@ -8,12 +13,34 @@ import HomePage from './pages/HomePage';
 import LBPage from './pages/LeaderboardPage';
 
 function App() {
+  const [userUID, setUserUID] = useState("");
+  const [userData, setUserData] = useState("");
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userUID) => {
+      setUserUID(auth?.currentUser?.uid);
+    })
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+      const userRef = ref(db, "/users");
+
+      const unsubscribe = onValue(userRef, (snapshot) => {
+          const data = snapshot.val();
+          setUserData(data);
+      })
+
+      return () => unsubscribe();
+  },[]);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />}/>
+        <Route path={`/${userUID}`} element={<HomePage />}/>
         <Route path='/login' element={<AuthPage />}/>
-        <Route path='/leaderboard' element={<LBPage />}/>
+        <Route path={`/leaderboard/${userUID}`} element={<LBPage userData={userData}/>}/>
       </Routes>
     </BrowserRouter>
   )
